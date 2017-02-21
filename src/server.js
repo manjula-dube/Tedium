e327da68567791c900f67261e78a9fccaae51aa4
manjula91 <dube.manjula668@gmail.com>
2017-02-21 23:00:43 +0530

    server side templating with nunjucks is working

--- src/server.js
+++ src/server.js
@@ -2,8 +2,17 @@
 
 import express from 'express'
 import bodyParser from 'body-parser'
+import nunjucks from 'nunjucks'
 
 let app = express()
+
+app.use(express.static(`${__dirname}/public`))
+app.set('view engine', 'html')
+
+nunjucks.configure(`${__dirname}/views`, {
+  autoescape: true,
+  express   : app
+})
 
 const config = {
   port: 3001,
@@ -11,7 +20,15 @@ const config = {
 }
 
 app.get('/', (req, res) => {
-  res.send('It Works!')
+  res.render('index', {
+    title : 'My First Nunjucks Page',
+    articles : [
+      { name : 'item #1', body: 'lorem upsdfg' },
+      { name : 'item #2', body: 'lorem upsdfg' },
+      { name : 'item #3', body: 'lorem upsdfg' },
+      { name : 'item #4', body: 'lorem upsdfg' },
+    ]
+  });
 })
 
 app.listen(config.port, () => {
